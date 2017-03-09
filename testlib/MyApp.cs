193a1fe96cc2ac8631f64f32066f8ac9c0a63586
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2017-03-09 11:58:41 +0100

    Unbroke the broken thingy...

---
+++ testlib/MyApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+
+namespace testlib
+{
+	[Application(Theme = "@style/MyTheme")]
+	public class MyApp : Application
+	{
+		public MyApp()
+		{
+		}
+	}
+}
---
+++ testlib/WorkingActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace testlib
+{
+	[Activity(Label = "WorkingActivity", MainLauncher = true)]
+	public class WorkingActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.WorkingLayout);
+		}
+	}
+}
